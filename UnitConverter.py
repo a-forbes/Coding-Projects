3cad02800e99a7ddaefda4a32163ba0c71e64df8
Andrew Forbes <andrew.forbes.9@gmail.com>
2019-07-05 11:31:12 -0400

    fixed typo

--- UnitConverter.py
+++ UnitConverter.py
@@ -8,7 +8,7 @@ if selection == 'F' or selection == 'f':
     print(str(temp) + ' degrees Fahrenheit is ' + str(round(((int(temp)-32)*(5/9)),2)) + ' degrees Celsius')
 elif selection == 'C' or selection == 'c':
     print('You have chosen Celsius to Fahrenheit conversion.')
-    temp = input('Please enter a temperature in Celsius.')
+    temp = input('Please enter a temperature in Celsius: ')
     print(str(temp) + ' degrees Celsius is ' + str(round(((int(temp)*(9/5))+32),2))+ ' degrees Fahrenheit')
 else:
     print('Invalid selection')
